9eedac73693ac9b00689833628000abc2ef3638a
Acreman <acreman@sina.com>
2020-07-30 08:35:45 +0800

    修改bug

--- ExaminationSystem/urls.py
+++ ExaminationSystem/urls.py
@@ -2,25 +2,20 @@ from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework import routers
 from django.urls import path,include
 import xadmin
-from rest_framework.routers import DefaultRouter
 from organization.views import *
-from users.views import *
-
-routers = DefaultRouter()
-routers.register('message', MessageView)
-routers.register('user', UserView)
-routers.register('class', ClassifyView)
-routers.register('topic', TopicView)
-routers.register('score', ScoreView)
-
 from users.views import UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users',UserViewSet)
+router.register('message', MessageView)
+router.register('class', ClassifyView)
+router.register('topic', TopicView)
+router.register('score', ScoreView)
+
 
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
-    path('', include(routers.urls))
+    path('', include(router.urls)),
     path('api-path/',include('rest_framework.urls'),name='restFramework'),
     path('login/', obtain_jwt_token),
 ]
